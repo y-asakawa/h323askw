4685a506bc07b3966f81f18d0d7dc15b8784091d
Yoshifumi ASAKAWA <36257330+y-asakawa@users.noreply.github.com>
2026-02-04 10:59:23 +0900

    改良前バックアップ

--- version.h
+++ version.h
@@ -15,9 +15,9 @@
 #define _H323ASKW_VERSION_H
 
 #define MAJOR_VERSION 1
-#define MINOR_VERSION 4
+#define MINOR_VERSION 5
 #define BUILD_TYPE    ReleaseCode
-#define BUILD_NUMBER 2
+#define BUILD_NUMBER 1
 
 
 #endif  // _H323ASKW_VERSION_H
