e4852332b87370733b8b45ec4a8987518b748e14
Yoshifumi ASAKAWA <36257330+y-asakawa@users.noreply.github.com>
2026-02-10 08:56:23 +0900

    Add version update and recording/video design docs

--- version.h
+++ version.h
@@ -14,10 +14,10 @@
 #ifndef _H323ASKW_VERSION_H
 #define _H323ASKW_VERSION_H
 
-#define MAJOR_VERSION 1
-#define MINOR_VERSION 5
+#define MAJOR_VERSION 2
+#define MINOR_VERSION 0
 #define BUILD_TYPE    ReleaseCode
-#define BUILD_NUMBER 2
+#define BUILD_NUMBER 1
 
 
 #endif  // _H323ASKW_VERSION_H
